cc950bbeb76766f805b525deae9c2091b6f76f76
yuki <aitev.yy0728@gmail.com>
2020-07-22 17:33:01 +0900

    deviseを導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  # app/views/devise/shared/_links.html.erb (リンク用パーシャル)
+  # app/views/devise/confirmations/new.html.erb (認証メールの再送信画面)
+  # app/views/devise/passwords/edit.html.erb (パスワード変更画面)
+  # app/views/devise/passwords/new.html.erb (パスワードを忘れた際、メールを送る画面)
+  # app/views/devise/registrations/edit.html.erb (ユーザー情報変更画面)
+  # app/views/devise/registrations/new.html.erb (ユーザー登録画面)
+  # app/views/devise/sessions/new.html.erb (ログイン画面)
+  # app/views/devise/unlocks/new.html.erb (ロック解除メール再送信画面)
+  # app/views/devise/mailer/confirmation_instructions.html.erb (メール用アカウント認証文)
+  # app/views/devise/mailer/password_change.html.erb （メール用パスワード変更完了文）
+  # app/views/devise/mailer/reset_password_instructions.html.erb (メール用パスワードリセット文)
+  # app/views/devise/mailer/unlock_instructions.html.erb (メール用ロック解除文)
 end
