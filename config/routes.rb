e77bab78e36349698976a6c258da4b35b27b95d0
yuki <aitev.yy0728@gmail.com>
2020-07-26 18:11:29 +0900

    deviseeサインイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+  get 'pages/show'
   devise_for :users
-  # app/views/devise/shared/_links.html.erb (リンク用パーシャル)
-  # app/views/devise/confirmations/new.html.erb (認証メールの再送信画面)
-  # app/views/devise/passwords/edit.html.erb (パスワード変更画面)
-  # app/views/devise/passwords/new.html.erb (パスワードを忘れた際、メールを送る画面)
-  # app/views/devise/registrations/edit.html.erb (ユーザー情報変更画面)
-  # app/views/devise/registrations/new.html.erb (ユーザー登録画面)
-  # app/views/devise/sessions/new.html.erb (ログイン画面)
-  # app/views/devise/unlocks/new.html.erb (ロック解除メール再送信画面)
-  # app/views/devise/mailer/confirmation_instructions.html.erb (メール用アカウント認証文)
-  # app/views/devise/mailer/password_change.html.erb （メール用パスワード変更完了文）
-  # app/views/devise/mailer/reset_password_instructions.html.erb (メール用パスワードリセット文)
-  # app/views/devise/mailer/unlock_instructions.html.erb (メール用ロック解除文)
+  # app/views/users/shared/_links.html.erb (リンク用パーシャル)
+  # app/views/users/confirmations/new.html.erb (認証メールの再送信画面)
+  # app/views/users/passwords/edit.html.erb (パスワード変更画面)
+  # app/views/users/passwords/new.html.erb (パスワードを忘れた際、メールを送る画面)
+  # app/views/users/registrations/edit.html.erb (ユーザー情報変更画面)
+  # app/views/users/registrations/new.html.erb (ユーザー登録画面)
+  # app/views/users/sessions/new.html.erb (ログイン画面)
+  # app/views/users/unlocks/new.html.erb (ロック解除メール再送信画面)
+  # app/views/users/mailer/confirmation_instructions.html.erb (メール用アカウント認証文)
+  # app/views/users/mailer/password_change.html.erb （メール用パスワード変更完了文）
+  # app/views/users/mailer/reset_password_instructions.html.erb (メール用パスワードリセット文)
+  # app/views/users/mailer/unlock_instructions.html.erb (メール用ロック解除文)
 end
