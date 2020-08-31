99d2a171f66d0ce16dad58e4310d12eb918aa35a
yuki <aitev.yy0728@gmail.com>
2020-09-01 08:51:32 +0900

    request_specを追加

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module BoardgameMatch
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.i18n.default_locale = :ja
+
     config.generators do |g|
       g.javascripts false
       g.stylesheets false
