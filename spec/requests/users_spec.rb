d4cba945d84cd03b0424e5f51319e08e4f027105
yuki <aitev.yy0728@gmail.com>
2020-09-04 02:37:33 +0900

    specを追加及びuserにカラムを追加

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Users", type: :request do
   describe "GET /users" do
     let(:user) { create(:user) }
     let(:user_params) { attributes_for(:user) }
-    let(:invalid_user_params) { attributes_for(:user, name: "") }
+    let(:invalid_user_params) { attributes_for(:user, username: "") }
 
     describe 'POST #create' do
       before do
@@ -53,7 +53,28 @@ RSpec.describe "Users", type: :request do
 
         it 'エラーが表示されること' do
           post user_registration_path, params: { user: invalid_user_params }
-          expect(response.body).to include 'prohibited this user from being saved'
+          expect(response.body).to include 'エラーが発生したため ユーザ は保存されませんでした。'
+        end
+      end
+    end
+
+    describe 'GET #edit' do
+      subject { get edit_user_registration_path }
+
+      context 'ログインしている場合' do
+        before do
+          user.confirm
+          sign_in user
+        end
+
+        it 'リクエストが成功すること' do
+          is_expected.to eq 200
+        end
+      end
+
+      context 'ゲストの場合' do
+        it 'リダイレクトされること' do
+          is_expected.to redirect_to new_user_session_path
         end
       end
     end
