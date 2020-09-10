cd2d3adad6ecb502bc705ecf4e7a1e539deb675e
yuki <aitev.yy0728@gmail.com>
2020-09-10 19:27:44 +0900

    eventのeditアクションを作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -17,8 +17,12 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
-  
+
   def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
     @event = Event.find(params[:id])
   end
 
