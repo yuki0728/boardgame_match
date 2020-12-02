85018d45c6d5d6167835157113b8a1e84d4317ff
yuki <aitev.yy0728@gmail.com>
2020-12-02 18:26:08 +0900

    地図から検索機能を追加

---
+++ app/controllers/maps_controller.rb
@@ -0,0 +1,22 @@
+class MapsController < ApplicationController
+  def index
+    @events = Event.all.to_json.html_safe
+  end
+
+  def map
+    @search_params = event_search_params
+    if @search_params == {}
+      # 検索していない場合でもイベント一覧は表示する。
+      @events = Event.all.to_json.html_safe
+    else
+      @events = Event.event_search(@search_params).to_json.html_safe
+    end
+    respond_to do |format|
+      format.js { @events }
+    end
+  end
+
+  def event_search_params
+    params.fetch(:event_search, {}).permit(:name, :tag_list, :date, :keyword)
+  end
+end
