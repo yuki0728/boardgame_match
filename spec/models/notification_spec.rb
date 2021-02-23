bae20ed9149e7ba0a86c22022067e6812bb16862
yuki <aitev.yy0728@gmail.com>
2021-02-24 03:37:22 +0900

    モデルのテストを追加

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:notification) { create(:notification) }
+
+  it "notificationインスタンスが有効である" do
+    expect(notification).to be_valid
+  end
+
+  it "visiter_idがnilでも有効である" do
+    notification.visiter_id = nil
+    expect(notification).to be_valid
+  end
+
+  it "visited_idがnilでも有効である" do
+    notification.visited_id = nil
+    expect(notification).to be_valid
+  end
+
+  it "event_idがnilでも有効である" do
+    notification.event_id = nil
+    expect(notification).to be_valid
+  end
 end
--- spec/models/participation_spec.rb
+++ spec/models/participation_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Participation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+  let!(:other_user) { create_list(:user, 2) }
+  let!(:event) { create(:event, user_id: user.id, participant_limit: 2) }
+
+  it 'イベントの参加人数が参加人数上限以下の場合は参加できる' do
+    participation = build(:participation, user_id: user.id, event_id: event.id)
+    expect(participation).to be_valid
+  end
+
+  it 'イベントの参加人数が参加人数上限以上になる場合は参加できない' do
+    0.upto(1).each do |index|
+      create(:participation, user_id: other_user[index].id, event_id: event.id)
+    end
+    participation = build(:participation, user_id: user.id, event_id: event.id)
+    expect(participation).to be_invalid
+  end
 end
